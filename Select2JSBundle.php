d7b7b2b301428f0266bc7f714147ee154e4123af
Nemanja Andrejevic <nemanja@nmdesign.rs>
2016-02-05 11:14:27 +0100

    Initial version

---
+++ Select2JSBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+	namespace Select2JSBundle;
+	
+	use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+	use DataTablesBundle\DependencyInjection\DataTablesExtension;
+	
+	class Select2JSBundle extends Bundle
+	{
+	}
+
+// END
